d220d340055e5f86e87541f085fa91b38afac893
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-16 22:42:30 +0200

    restyled article:id page

--- src/View/Article/Article.tsx
+++ src/View/Article/Article.tsx
@@ -16,19 +16,23 @@ export default function Article() {
         <div className='page-article-container'>
             {article && <div className='page-article'>
                 <div>
-                    <img className='p-a-img' src={article.image_link}/>
+                    <h2 className='p-a-title' onClick={() => {
+                        navigate(`/article/${article.id}`)
+                    }}>{article.title}</h2>
+                    <div className = 'main-img-container'>
+                        <img className='p-a-img' src={article.image_link}/>
 
-                    <div className='tags p-a-tags'>
-                        {article.tags && article.tags.map(tag => {
-                            return <div key={tag.id} className='tag'>{tag.name}</div>
-                        })}
-                        <div className='a-date p-a-date'>{article.createdAt}</div>
+                        <div className='tags p-a-tags'>
+                            {article.tags && article.tags.map(tag => {
+                                return <div key={tag.id} className='tag'>{tag.name}</div>
+                            })}
+                            <div className='a-date p-a-date'>{article.createdAt}</div>
+                        </div>
                     </div>
+
                 </div>
 
-                <h2 className='p-a-title' onClick={() => {
-                    navigate(`/article/${article.id}`)
-                }}>{article.title}</h2>
+
                 {<BlocksRenderer content={article.body}/>}
 
 
