67ecbaf91418ea2188b9a670eac8f2eabbe04490
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-22 19:45:17 +0200

    redesigned logo / did some styling works

--- src/View/Header/header.tsx
+++ src/View/Header/header.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './index.css';
 import {useNavigate} from "react-router-dom";
+import Logo from "./logo";
 
 export default function Header() {
     const navigate = useNavigate()
     return (
+
         <div className = 'Header'>
-            <div className = 'logo'>PawelCzekaj.pl</div>
+            <Logo/>
             <ul>
-                <li className = 'hov' onClick = {()=>{navigate('/')}}>Home</li>
-                <li className = 'hov' onClick = {()=>{navigate('/articles')}}>Articles</li>
-                <li className = 'hov' onClick = {()=>{navigate('/about')}}>About Me</li>
-                <li className = 'hov' onClick = {()=>{navigate('/contact')}}>Contact</li>
+
+                    <li className = 'hov' onClick = {()=>{navigate('/')}}>Home</li>
+                    <li className = 'hov' onClick = {()=>{navigate('/articles')}}>Articles</li>
+                    <li className = 'hov' onClick = {()=>{navigate('/about')}}>About Me</li>
+                    <li className = 'hov' onClick = {()=>{navigate('/contact')}}>Contact</li>
+
+
             </ul>
         </div>
     )
---
+++ src/View/Header/logo.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './index.css';
+
+export default function Logo() {
+    return (
+
+        <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 176 73.8">
+            <g id="Layer_1-2" data-name="Layer 1">
+                <g>
+                    <text transform="translate(84.54 34.5) scale(.76 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="15.64" fontWeight="500"><tspan x="0" y="0" letter-spacing="0em">P</tspan><tspan x="9.65" y="0" letter-spacing=".03em">A</tspan><tspan x="20.44" y="0" letter-spacing=".06em">WEŁ </tspan><tspan x="58.96" y="0" letter-spacing=".06em">C</tspan><tspan x="70.41" y="0" letter-spacing=".06em">ZE</tspan><tspan x="90.11" y="0" letter-spacing=".04em">K</tspan><tspan x="100.57" y="0" letter-spacing=".05em">A</tspan><tspan x="111.69" y="0" letter-spacing=".06em">J</tspan></text>
+                    <text transform="translate(84.54 49.4) scale(.85 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="11.4" fontWeight="300" letter-spacing=".06em"><tspan x="0" y="0">P</tspan></text>
+                    <text transform="translate(90.6 49.4) scale(.59 .7)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="11.4" fontWeight="300" letter-spacing=".06em"><tspan x="0" y="0">ERSONAL</tspan></text>
+                    <text transform="translate(121.07 49.4) scale(.85 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="11.4" fontWeight="300" letter-spacing=".06em"><tspan x="0" y="0" > B</tspan></text>
+                    <text transform="translate(129.9 49.4) scale(.59 .7)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="11.4" fontWeight="300"><tspan x="0" y="0" letter-spacing=".02em">L</tspan><tspan x="5.88" y="0" letter-spacing=".06em">O</tspan><tspan x="14.02" y="0" letter-spacing=".06em">G</tspan></text>
+                </g>
+                <g>
+                    <rect x=".5" y="8.48" width="58" height="58" fill="none" stroke="#fff" strokeMiterlimit="10"/>
+                    <text transform="translate(2.76 33.4)" fill="#fff" fontFamily="loos-normal" fontSize="40" fontWeight="700"><tspan x="0" y="0">p</tspan></text>
+                    <text transform="translate(32.76 63.4)" fill="#fff" fontFamily="loos-normal" fontSize="40" fontWeight="700"><tspan x="0" y="0">c</tspan></text>
+                </g>
+            </g>
+        </svg>
+        // <svg id="Layer_2" data-name="Layer 2" viewBox="0 0 169 73.8">
+        //     <g id="Layer_1-2" data-name="Layer 1">
+        //         <g>
+        //             <text transform="translate(84.54 34.68) scale(.76 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="14.44" fontWeight="500"><tspan x="0" y="0" letter-spacing="0em">P</tspan><tspan x="8.91" y="0" letter-spacing=".03em">A</tspan><tspan x="18.87" y="0" letter-spacing=".06em">WEŁ </tspan><tspan x="54.45" y="0" letter-spacing=".05em">C</tspan><tspan x="65.02" y="0" letter-spacing=".06em">ZE</tspan><tspan x="83.21" y="0" letter-spacing=".04em">K</tspan><tspan x="92.88" y="0" letter-spacing=".05em">A</tspan><tspan x="103.14" y="0" letter-spacing=".06em">J</tspan></text>
+        //             <text transform="translate(84.54 48.44) scale(.85 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="10.52" fontWeight="300"><tspan x="0" y="0">P</tspan></text>
+        //             <text transform="translate(89.58 48.44) scale(.59 .7)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="10.52" fontWeight="300"><tspan x="0" y="0">ERSONAL</tspan></text>
+        //             <text transform="translate(115.01 48.44) scale(.85 1)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="10.52" fontWeight="300"><tspan x="0" y="0" > B</tspan></text>
+        //             <text transform="translate(122.05 48.44) scale(.59 .7)" fill="#e6e6e6" fontFamily="loos-normal" fontSize="10.52" fontWeight="300"><tspan x="0" y="0" letter-spacing="-.04em">L</tspan><tspan x="4.78" y="0" letter-spacing="0em">O</tspan><tspan x="11.64" y="0">G</tspan></text>
+        //         </g>
+        //         <g>
+        //             <rect x=".5" y="8.48" width="58" height="58" fill="none" stroke="#fff" strokeMiterlimit="10"/>
+        //             <text transform="translate(2.76 33.4)" fill="#fff" fontFamily="loos-normal" fontSize="40" fontWeight="700"><tspan x="0" y="0">p</tspan></text>
+        //             <text transform="translate(32.76 63.4)" fill="#fff" fontFamily="loos-normal" fontSize="40" fontWeight="700"><tspan x="0" y="0">c</tspan></text>
+        //         </g>
+        //     </g>
+        // </svg>
+
+    // <svg id="Layer_2" data-name="Layer 2"  viewBox="0 0 168.42 59">
+    //     <g id="Layer_1-2" data-name="Layer 1">
+    //         <g>
+    //             <g>
+    //                 <path d="M88.21,17.03c1.77,0,2.59.98,2.59,2.89v.5c0,1.83-.82,2.87-2.59,2.87h-1.6v3.41h-1.39v-9.67h2.99ZM86.61,21.75h1.54c.83,0,1.25-.38,1.25-1.37v-.42c0-.97-.42-1.4-1.25-1.4h-1.54v3.19Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M95.95,17.03l2.41,9.67h-1.39l-.59-2.38h-3.02l-.58,2.38h-1.29l2.37-9.67h2.09ZM93.73,22.8h2.26l-.98-3.96h-.31l-.97,3.96Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M100.41,17.03l1.06,7.65h.31l1.09-7.65h2.07l1.1,7.65h.33l1-7.65h1.24l-1.27,9.67h-2.22l-1.12-7.65h-.31l-1.09,7.65h-2.18l-1.33-9.67h1.32Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M115.29,17.03v1.53h-3.68v2.47h3.16v1.49h-3.16v2.66h3.73v1.53h-5.12v-9.67h5.06Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M118.63,17.03v3.71l.96-1.02v1.6l-.96,1.02v2.83h3.41v1.53h-4.8v-2.87l-.69.74v-1.62l.69-.74v-5.18h1.39Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M129.6,16.88c1.83,0,3,1.27,3,3.72v.16h-1.39v-.16c-.01-1.44-.61-2.14-1.62-2.14h-.2c-1,0-1.64.72-1.64,2.27v2.22c0,1.62.64,2.3,1.64,2.3h.21c.99,0,1.64-.66,1.64-2.17v-.1h1.4v.1c0,2.54-1.2,3.75-3.04,3.75h-.21c-1.82,0-3.04-1.23-3.04-3.88v-2.22c0-2.56,1.22-3.85,3.04-3.85h.21Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M139.4,17.03v1.67l-3.57,6.06v.4h3.64v1.53h-5.26v-1.67l3.55-6.06v-.4h-3.48v-1.53h5.11Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M146.33,17.03v1.53h-3.68v2.47h3.16v1.49h-3.16v2.66h3.73v1.53h-5.12v-9.67h5.07Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M149.67,17.03v3.97h.33l2.56-3.97h1.53l-2.83,4.4v.43l3.03,4.84h-1.69l-2.59-4.14h-.34v4.14h-1.39v-9.67h1.39Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M159.57,17.03l2.41,9.67h-1.39l-.59-2.38h-3.02l-.58,2.38h-1.29l2.37-9.67h2.09ZM157.35,22.8h2.26l-.98-3.96h-.31l-.97,3.96Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M168.42,17.03v6.48c0,2.24-.94,3.33-2.56,3.33h-.13c-1.63,0-2.57-1.1-2.57-3.33v-1.28h1.39v1.31c0,1.15.41,1.72,1.18,1.72h.12c.78,0,1.18-.56,1.18-1.72v-4.98h-3.71v-1.53h5.1Z" fill="#e6e6e6" stroke-width="0"/>
+    //             </g>
+    //             <g>
+    //                 <path d="M87.22,33.41c1.26,0,1.89.61,1.89,1.87v.52c0,1.18-.65,1.86-1.89,1.86h-1.5v2.8h-.59v-7.05h2.09ZM85.72,37.06h1.5c.87,0,1.3-.42,1.3-1.26v-.52c0-.85-.42-1.28-1.3-1.28h-1.5v3.06Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M92.64,35.52v.41h-2.23v1.81h1.96v.42h-1.96v1.88h2.26v.41h-2.67v-4.94h2.64Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M94.85,35.52c.83,0,1.35.42,1.35,1.3v.31c0,.65-.32,1.08-.86,1.23l1.03,2.1h-.45l-1-2.03h-1.09v2.03h-.41v-4.94h1.43ZM93.83,38.01h.99c.6,0,.96-.27.96-.88v-.31c0-.64-.36-.88-.96-.88h-.99v2.08Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M98.35,35.46c.85,0,1.4.51,1.4,1.33v.55h-.42v-.55c0-.56-.34-.91-.98-.91h-.08c-.64,0-.98.35-.98.84v.1c0,.5.34.66,1.16.95.96.35,1.37.68,1.37,1.42v.09c0,.71-.49,1.25-1.47,1.25h-.07c-.98,0-1.46-.54-1.46-1.38v-.53h.41v.53c0,.63.34.97,1.05.97h.08c.7,0,1.05-.34,1.05-.83v-.09c0-.48-.3-.71-1.16-1.01-.88-.29-1.38-.57-1.38-1.36v-.1c0-.75.5-1.25,1.4-1.25h.08Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M102.21,35.46c1.03,0,1.65.69,1.65,1.97v1.12c0,1.28-.62,1.97-1.65,1.97h-.08c-1.03,0-1.66-.69-1.66-1.97v-1.12c0-1.28.63-1.97,1.66-1.97h.08ZM100.88,37.43v1.12c0,1,.48,1.55,1.25,1.55h.08c.78,0,1.24-.55,1.24-1.55v-1.12c0-1-.46-1.55-1.24-1.55h-.08c-.77,0-1.25.55-1.25,1.55Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M105.31,35.52l1.85,4.21h.12v-4.21h.39v4.94h-.69l-1.84-4.21h-.12v4.21h-.39v-4.94h.68Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M110.31,35.52l1.49,4.94h-.43l-.46-1.52h-1.84l-.45,1.52h-.39l1.46-4.94h.61ZM109.2,38.53h1.59l-.72-2.39h-.16l-.71,2.39Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M112.76,35.52v4.53h2.11v.41h-2.52v-4.94h.41Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M119.57,33.41c1.29,0,1.83.63,1.83,1.61v.38c0,.62-.27,1.06-.7,1.32v.24c.5.22.89.67.89,1.48v.28c0,1.05-.56,1.74-1.89,1.74h-2.26v-7.05h2.12ZM118.03,36.61h1.59c.83,0,1.19-.47,1.19-1.15v-.36c0-.67-.29-1.12-1.24-1.12h-1.54v2.62ZM118.03,39.88h1.67c.97,0,1.29-.46,1.29-1.22v-.29c0-.79-.46-1.2-1.37-1.2h-1.6v2.72Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M122.89,35.52v4.53h2.11v.41h-2.52v-4.94h.41Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M126.98,35.46c1.03,0,1.65.69,1.65,1.97v1.12c0,1.28-.62,1.97-1.65,1.97h-.08c-1.03,0-1.66-.69-1.66-1.97v-1.12c0-1.28.62-1.97,1.66-1.97h.08ZM125.66,37.43v1.12c0,1,.47,1.55,1.24,1.55h.08c.78,0,1.24-.55,1.24-1.55v-1.12c0-1-.46-1.55-1.24-1.55h-.08c-.77,0-1.24.55-1.24,1.55Z" fill="#e6e6e6" stroke-width="0"/>
+    //                 <path d="M131.02,35.46c.95,0,1.56.62,1.56,1.84v.12h-.42v-.12c0-.96-.42-1.42-1.14-1.42h-.12c-.71,0-1.16.46-1.16,1.44v1.33c0,1.01.47,1.44,1.16,1.44h.12c.69,0,1.16-.42,1.16-1.39v-.26h.44v2.01h-.31v-.59h-.16c-.26.43-.69.65-1.18.65h-.12c-.89,0-1.53-.58-1.53-1.86v-1.33c0-1.25.62-1.86,1.58-1.86h.12Z" fill="#e6e6e6" stroke-width="0"/>
+    //             </g>
+    //         </g>
+    //         <g>
+    //             <path d="M59,59H0V0h59v59ZM1,58h57V1H1v57Z" fill="#fff" stroke-width="0"/>
+    //             <path d="M11.2,4.9v2.2h.84c1.32-1.64,3.24-2.6,5.84-2.6h.56c4.64,0,7.72,2.84,7.72,9.08v3.12c0,6.24-3.08,9.12-7.72,9.12h-.56c-2.64,0-4.56-1-5.84-2.72h-.84v11.92h-6.16V4.9h6.16ZM11.2,13.5v3.32c0,2.72,1.56,4.08,4.12,4.08h.56c2.48,0,4.12-1.36,4.12-4.08v-3.32c0-2.72-1.64-4.08-4.12-4.08h-.56c-2.52,0-4.12,1.36-4.12,4.08Z" fill="#fff" stroke-width="0"/>
+    //             <path d="M45.28,34.5c5.84,0,9.8,2.84,9.8,8.24v.52h-6.04v-.52c0-1.96-1.28-3.32-3.76-3.32h-.56c-2.56,0-3.92,1.48-3.92,4.04v3.48c0,2.48,1.36,3.96,3.92,3.96h.56c2.48,0,3.88-1.36,3.88-3.24v-.44h6v.44c0,5.2-4,8.16-9.88,8.16h-.56c-6,0-10.08-3.08-10.08-9.08v-3.12c0-6.04,4.08-9.12,10.08-9.12h.56Z" fill="#fff" stroke-width="0"/>
+    //         </g>
+    //     </g>
+    // </svg>
+    )
+}
