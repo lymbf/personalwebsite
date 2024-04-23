e238ef4756c2f0d4b56d34d5cdab753479b2249e
Paweł Czekaj <paw.czekaj@gmail.com>
2024-04-23 17:21:55 +0200

    added basic about me page with basic styling

--- src/View/About/About.tsx
+++ src/View/About/About.tsx
@@ -4,7 +4,12 @@ import './index.css';
 export default function About() {
     return (
         <div className = 'About'>
-            asd
+            <div className = 'about-container'>
+                <h2>Subiektywnie obiektywny</h2>
+                <h3>Taki tam gostek</h3>
+                <h2>coś sobie sprawdze, coś sobie zbadam..</h2>
+                <div className = 'avatar-circle'></div>
+            </div>
         </div>
     )
 }
